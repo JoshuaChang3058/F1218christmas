b2749712a7474ac52459c7347a39c671728565d8
dafeng306 <75168543+dafeng306@users.noreply.github.com>
2020-12-18 08:31:59 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -107,15 +107,15 @@ function 第一段 () {
     music.playTone(587, music.beat(BeatFraction.Whole))
     music.playTone(523, music.beat(BeatFraction.Whole))
     music.playTone(392, music.beat(BeatFraction.Double))
-    music.rest(music.beat(BeatFraction.Whole))
-    music.rest(music.beat(BeatFraction.Half))
-    music.playTone(392, music.beat(BeatFraction.Whole))
     music.playTone(659, music.beat(BeatFraction.Whole))
     music.playTone(587, music.beat(BeatFraction.Whole))
     music.playTone(523, music.beat(BeatFraction.Whole))
     music.playTone(440, music.beat(BeatFraction.Double))
     music.rest(music.beat(BeatFraction.Whole))
     music.rest(music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(392, music.beat(BeatFraction.Whole))
 }
 basic.forever(function () {
     第一段()
